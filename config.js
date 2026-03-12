8d2dce8b3f41a835c85209923d1f78e3b2d1f07f
shah-bot12 <warisbot74@gmail.com>
2026-03-13 00:34:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     botName: "𝐒𝐇𝐀𝐇-𝐌𝐃 ☠️",
     ownerName: "𝐖𝐀𝐑𝐈𝐒 𝐒𝐇𝐀𝐇",
-    ownerNumber: "923191969315", // Bina '+' ke apna number
+    ownerNumber: "923556524069", // Bina '+' ke apna number
     prefix: ".",
     public_mode: true
 };
